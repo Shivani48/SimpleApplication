773b8e870b68c68b5becb79e21c526ccbd015709
Shivani Dhanasamy <shivanidhanasamy04@gmail.com>
2019-01-30 11:55:46 -0500

    Final

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "hi how was your day", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "So tell me a joke !", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
